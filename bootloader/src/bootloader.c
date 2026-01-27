7a5ab9f0f4305bec67de6235c890e85b776f1102
kavan6 <kavanheppenstall@gmail.com>
2026-01-27 20:05:23 +0000

    Clean bootloader jump function

--- bootloader/src/bootloader.c
+++ bootloader/src/bootloader.c
@@ -1,4 +1,5 @@
 #include <libopencm3/stm32/memorymap.h>
+#include <libopencm3/cm3/vector.h>
 
 #include "common-defines.h"
 
@@ -7,13 +8,8 @@
 
 static void jump_to_main(void)
 {
-    typedef void (*void_fn)(void);
-
-    uint32_t* reset_vector_entry = (uint32_t*)(MAIN_APP_START_ADDRESS + 4U);
-    uint32_t* reset_vector = (uint32_t*)(*reset_vector_entry);
-    void_fn jump_fn = (void_fn)reset_vector;
-
-    jump_fn();
+    vector_table_t* main_vector_table = (vector_table_t*)(MAIN_APP_START_ADDRESS);
+    main_vector_table->reset();
 }
 
 int main(void)
